8d4f62ea5a5a4784d048a1bc2b928e0822c25a74
EslamEed01 <109042007+EslamEed01@users.noreply.github.com>
2023-02-20 08:45:49 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1772A | A+B?/185811296.cpp
@@ -0,0 +1,22 @@
+// //-------------------------------------------------------------------
+// //======================= Eslam_Ee0 =================================
+#include<bits/stdc++.h>
+using namespace std;
+#define eslam_eed ios::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ll long long;
+//ويسر لى امرى 
+int main()
+{
+    eslam_eed;
+    int a,b;
+    char c;
+    int t;cin>>t;
+    while(t--)
+    {
+        cin>>a>>c>>b;
+        cout<<a+b<<endl;
+ 
+    }
+ 
+    
+}
